f9e8585ac54abe5903f8dbd7ac8f1e24539a146b
Sebaperez12 <sebaperez12@gmail.com>
2025-10-18 01:02:56 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+function datoRandom(arreglo) {
+
+  const indiceAleatorio = Math.floor(Math.random() * arreglo.length);
+    
+    return arreglo[indiceAleatorio];
+    
+}
+
+let resultado = datoRandom(who) + " " + datoRandom(action) + " " + datoRandom(what) + " " + datoRandom(when); 
+
+document.getElementById("excuse").innerText = resultado;
